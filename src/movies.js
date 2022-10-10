dcd27c7fd209bf66c70f01dd7917770aff7ce2ad
John Doe <atarod.shirin@gmail.com>
2022-10-10 15:12:01 +0200

    iteration2 done

--- src/movies.js
+++ src/movies.js
@@ -7,22 +7,38 @@ function getAllDirectors(moviesArray) {
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+
+    let dramaArr = moviesArray.filter((movie) => {
+
+        const genreArr = movie.genre;
+
+        for (let i = 0; i < genreArr.length; i += 1) {
+            if (genreArr[i] === 'Drama') {
+                return true;
+            }
+        }
+git 
+    }).filter(movie => movie.director === 'Steven Spielberg');
+
+    return dramaArr.length;
+}
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) { }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) { }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) { }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) { }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) { }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) { }
